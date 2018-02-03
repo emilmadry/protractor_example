256cda703c5f7c784dc6e64158492d3a7b0ffba1
emilmadry <emil.madry91@gmail.com>
2018-02-03 19:18:37 +0100

    eslint protractor and jasmine added

--- tests/spec.js
+++ tests/spec.js
@@ -6,38 +6,38 @@ describe('Protractor Demo App', function() {
 
     beforeEach(function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-      });
+    });
 
-      it('should have a title', function() {
+    it('should have a title', function() {
         expect(browser.getTitle()).toEqual('Super Calculator');
-      });
+    });
 
-      it('should add one and two', function() {
+    it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
     
         goButton.click();
     
         expect(latestResult.getText()).toEqual('3');
-      });
+    });
 
     it('should add one and two', function() {
-      element(by.model('first')).sendKeys(1);
-      element(by.model('second')).sendKeys(2);
+        element(by.model('first')).sendKeys(1);
+        element(by.model('second')).sendKeys(2);
   
-      goButton.click();
+        goButton.click();
   
-      expect(element(by.binding('latest')).getText()).
-          toEqual('3'); // This is correct
+        expect(element(by.binding('latest')).getText()).
+            toEqual('3'); // This is correct
     });
 
     it('should add four and six', function() {
         // Fill this in.
         expect(latestResult.getText()).toEqual('10');
-      });
+    });
     
-      it('should read the value from an input', function() {
+    it('should read the value from an input', function() {
         firstNumber.sendKeys(1);
         expect(firstNumber.getAttribute('value')).toEqual('1');
-      });
-  });
+    });
+});
